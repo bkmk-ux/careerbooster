52ad1fe7c748a00573dbb4b1a5c50a45a0ca12cc
Mehrez <bkmk29947@gmail.com>
2026-04-25 01:11:13 +0100

    Add register and login authentication with bcrypt

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const userRoutes = require("./routes/userRoutes");
 require("dotenv").config();
 
 const app = express();
@@ -9,15 +10,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//Routes
+app.use("/api/users", userRoutes);
 // Test Route
 app.get("/", (req, res) => {
   res.send("API running...");
 });
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 5000;
 
